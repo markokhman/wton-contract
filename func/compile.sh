c4a9438fbf1ca083d23fd22f662e7f598ea14cff
Nick Nekilov <97469315+NickNekilov@users.noreply.github.com>
2022-12-01 18:38:52 +0100

    move/rename files

--- func/compile.sh
+++ func/compile.sh
@@ -1,8 +1,5 @@
-func -SPA -o ./build/jetton-wallet.fif common/stdlib.fc params.fc op-codes.fc jetton-utils.fc jetton-wallet.fc
+func -SPA -o ./build/jetton-wallet.fif common/stdlib.fc common/chains.fc common/op-codes.fc common/packers.fc jetton-wallet.fc
 echo '"build/jetton-wallet.fif" include 2 boc+>B "build/jetton-wallet.boc" B>file' | fift -s
-func -SPA -o ./build/jetton-minter.fif common/stdlib.fc params.fc op-codes.fc jetton-utils.fc jetton-minter.fc
-func -SPA -o ./build/jetton-minter-ICO.fif common/stdlib.fc params.fc op-codes.fc jetton-utils.fc jetton-minter-ICO.fc
-func -SPA -o ./build/jetton-discovery.fif common/stdlib.fc params.fc op-codes.fc discovery-params.fc jetton-utils.fc jetton-discovery.fc
-func -SPA -o ./build/jetton-minter-discoverable.fif common/stdlib.fc params.fc op-codes.fc discovery-params.fc jetton-utils.fc jetton-minter-discoverable.fc
+func -SPA -o ./build/jetton-root.fif common/stdlib.fc common/chains.fc common/op-codes.fc common/packers.fc jetton-root.fc
 
 fift -s build/print-hex.fif
